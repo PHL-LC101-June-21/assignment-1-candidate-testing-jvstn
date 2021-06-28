117f16b42c3551458332cee36efa014bb959c16b
Justin McFadden <56053152+jvstn@users.noreply.github.com>
2021-06-28 20:10:34 +0000

    Typo in first question

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,11 +5,11 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who is the first American woman in space? ";
+let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions = [
-  "Who is first American woman in space? ",
+  "Who was first American woman in space? ",
   "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ",
   "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
   "What is the minimum crew size for the ISS? "
@@ -57,6 +57,7 @@ function gradeQuiz(candidateAnswers) {
     console.log('false');
   }
   console.log(`Your grade is ${grade}`)
+
   return grade;
 }
 
