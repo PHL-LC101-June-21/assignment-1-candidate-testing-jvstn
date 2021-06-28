cb1b5aa692425fee101169ed7cbd25244a579d35
Justin McFadden <56053152+jvstn@users.noreply.github.com>
2021-06-28 19:51:47 +0000

    Finished without styling

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,37 +8,62 @@ let candidateName;
 let question;
 let correctAnswer;
 let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who is first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3"
+];
+let candidateAnswers = [];
 
 
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
+  // TODO 1.1b: Ask for candidate's name 
+  candidateName = input.question("What is your name? ");
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  for (question of questions) {
+    let answer = input.question(question);
+    candidateAnswers.push(answer);
+  }
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  let numCorrect = 0;
+  for (let i = 0; i < questions.length; i++) {
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+      numCorrect++;
+    }
+  }
 
+  let grade = (numCorrect / questions.length) * 100;
 
-  let grade;
-  
-
+  if (grade >= 80) {
+    console.log('Pass');
+  } else {
+    console.log('false');
+  }
+  console.log(`Your grade is ${grade}`)
   return grade;
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
